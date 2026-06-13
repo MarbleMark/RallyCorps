0f585e6177d41f34af0773cd1e88687c29ef0180
Mark Larson <35248539+MarbleMark@users.noreply.github.com>
2026-06-13 16:40:34 -0500

    Update rally_corps_volunteers.php
    Changed CurlToRC() to take the entire partner_rows array instead of a single record.

--- rally_corps_volunteers.php
+++ rally_corps_volunteers.php
@@ -93,52 +93,52 @@ function VolunteerToArray ( $db, $volunteer ): array {
 	}
 	//	Get the remaining user fields and add the history
 	$user = [
-		'users.pers_ID' => $volunteer->pers_ID,
-		'users.first_name' => $volunteer->first_name,
-		'users.last_name' => $volunteer->last_name,
-		'users.middle_initial' => $volunteer->m_i,
-		'users.email' => $volunteer->e_mail,
-		'users.address' => $volunteer->address,
-		'users.city' => $volunteer->city,
-		'users.state' => $volunteer->state,
-		'users.postal_code' => $volunteer->zip,
-		'users.country' => $volunteer->country,
-		'users.phone' => $volunteer->phone,
-		'users.call_sign' => $volunteer->call_sign,
-		'users.shirt_size' => $volunteer->shirt_size,
+		'personnel.pers_ID' => $volunteer->pers_ID,
+		'personnel.first_name' => $volunteer->first_name,
+		'personnel.last_name' => $volunteer->last_name,
+		'personnel.middle_initial' => $volunteer->m_i,
+		'personnel.e_mail' => $volunteer->e_mail,
+		'personnel.address' => $volunteer->address,
+		'personnel.city' => $volunteer->city,
+		'personnel.state' => $volunteer->state,
+		'personnel.postal_code' => $volunteer->zip,
+		'personnel.country' => $volunteer->country,
+		'personnel.phone' => $volunteer->phone,
+		'personnel.call_sign' => $volunteer->call_sign,
+		'personnel.shirt_size' => $volunteer->shirt_size,
 /* last_contact is the last time the person logged in to Rallymaster Pro */			
-		'users.last_contact' => $volunteer->last_contact,	
+		'personnel.last_contact' => $volunteer->last_contact,	
 /* private_comments are comments that can be seen by all organizers, but not by the volunteer. */			
 /* These are notes such as warnings about the volunteer not showing up without giving notice. */			
-		'users.private_comments' => $volunteer->comments,	
-		'users.dont_send_email' => $volunteer->dont_send_email,	
+		'personnel.private_comments' => $volunteer->comments,	
+		'personnel.dont_send_email' => $volunteer->dont_send_email,	
 /* last_event and last_event_date are not reliable.  They depend on the organizers recording the data */
-		'users.last_event' => $volunteer->last_event,	
-		'users.last_event_date' => $volunteer->last_event_date,	
+		'personnel.last_event' => $volunteer->last_event,	
+		'personnel.last_event_date' => $volunteer->last_event_date,	
 /* Volunteer preferences (specialties) */
-		'users.pref_registration' => $volunteer->registration,	
-		'users.pref_public_relations' => $volunteer->public_relations,	
-		'users.pref_hospitality' => $volunteer->hospitality,	
-		'users.pref_tech_inspection' => $volunteer->tech_inspection,	
-		'users.pref_bannering' => $volunteer->bannering,	
-		'users.pref_advance' => $volunteer->advance,	
-		'users.pref_course_marshal' => $volunteer->course_marshal,	
-		'users.pref_radio_operator' => $volunteer->radio_op,	
-		'users.pref_control_worker' => $volunteer->control_worker,	
-		'users.pref_stage_captain' => $volunteer->stage_captain,	
-		'users.pref_spec_captain' => $volunteer->spec_captain,	
-		'users.pref_spectator_marshal' => $volunteer->spectator,	
-		'users.pref_service' => $volunteer->service,	
-		'users.pref_vip_team' => $volunteer->vip,	
-		'users.pref_sweep' => $volunteer->sweep,	
-		'users.pref_medical' => $volunteer->medical,	
-		'users.medical_training' => $volunteer->med_training,	
-		'users.medical_cert' => $volunteer->med_cert,	
-		'users.medical_cert_num' => $volunteer->med_cert_num,	
-		'users.medical_cert_state' => $volunteer->med_cert_state,	
-		'users.medical_cert_expire' => $volunteer->med_cert_expire,	
-		'users.pref_other_specialties' => $volunteer->other_spec,	
-		'users.history' => $history
+		'personnel.pref_registration' => $volunteer->registration,	
+		'personnel.pref_public_relations' => $volunteer->public_relations,	
+		'personnel.pref_hospitality' => $volunteer->hospitality,	
+		'personnel.pref_tech_inspection' => $volunteer->tech_inspection,	
+		'personnel.pref_bannering' => $volunteer->bannering,	
+		'personnel.pref_advance' => $volunteer->advance,	
+		'personnel.pref_course_marshal' => $volunteer->course_marshal,	
+		'personnel.pref_radio_operator' => $volunteer->radio_op,	
+		'personnel.pref_control_worker' => $volunteer->control_worker,	
+		'personnel.pref_stage_captain' => $volunteer->stage_captain,	
+		'personnel.pref_spec_captain' => $volunteer->spec_captain,	
+		'personnel.pref_spectator_marshal' => $volunteer->spectator,	
+		'personnel.pref_service' => $volunteer->service,	
+		'personnel.pref_vip_team' => $volunteer->vip,	
+		'personnel.pref_sweep' => $volunteer->sweep,	
+		'personnel.pref_medical' => $volunteer->medical,	
+		'personnel.medical_training' => $volunteer->med_training,	
+		'personnel.medical_cert' => $volunteer->med_cert,	
+		'personnel.medical_cert_num' => $volunteer->med_cert_num,	
+		'personnel.medical_cert_state' => $volunteer->med_cert_state,	
+		'personnel.medical_cert_expire' => $volunteer->med_cert_expire,	
+		'personnel.pref_other_specialties' => $volunteer->other_spec,	
+		'personnel.history' => $history
 	];
 	return $user;
 }
@@ -147,9 +147,8 @@ function VolunteerToArray ( $db, $volunteer ): array {
 
 function VolunteerToRC($db, $volunteer): string {
 	//	Convert the volunteer record into an array
-	$partnerRows = VolunteerToArray ( $db, $volunteer );
+	$partnerRows[] = VolunteerToArray ( $db, $volunteer );
 	
-	//	Uncomment to debug
 //	echo '<pre>'; print_r ( $partnerRow ); echo '</pre><br>'; exit;
 	
 	//	Send to Rally Corp
@@ -160,12 +159,12 @@ function VolunteerToRC($db, $volunteer): string {
 //	Send an array of volunteers to Rally Corps
 
 function VolunteersToRC($db, $volunteers): string {	
+	$partnerRows = [];
 	//	Loop through every volunteer
 	foreach ( $volunteers as $volunteer ) {
 		$partnerRows[] = VolunteerToArray ( $db, $volunteer );
 	}
 
-	//	Uncomment to debug
 //	echo '<pre>'; print_r ( $partnerRows ); echo '</pre><br>'; exit;
 	
 	$url = 'https://rallycorps.onrender.com/v1/webhooks/import/users';
